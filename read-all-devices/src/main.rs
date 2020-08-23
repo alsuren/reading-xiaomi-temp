a5d429ad6d0e3fa91707e73bbfe59592faa1a744
David Laban <alsuren@gmail.com>
2020-08-23 21:47:47 +0100

    react to new bluetooth addresses from all events

--- read-all-devices/src/main.rs
+++ read-all-devices/src/main.rs
@@ -24,7 +24,7 @@ fn main() {
     println!("waiting");
     let mut seen = Default::default();
     while let Ok(event) = event_receiver.recv() {
-        match on_event(&central, event, &mut seen) {
+        match on_event(&central, &mut seen, event) {
             Ok(()) => {}
             Err(err) => {
                 dbg!(err);
@@ -35,42 +35,54 @@ fn main() {
 
 fn on_event(
     central: &ConnectedAdapter,
-    event: CentralEvent,
     seen: &mut HashSet<BDAddr>,
+    event: CentralEvent,
 ) -> Result<(), Box<dyn Error + Send + Sync>> {
     match event {
         CentralEvent::DeviceDiscovered(bd_addr) => {
             println!("DeviceDiscovered: {:?}", bd_addr);
+            handle_bd_addr(central, seen, bd_addr)?;
         }
         CentralEvent::DeviceConnected(bd_addr) => {
             println!("DeviceConnected: {:?}", bd_addr);
+            handle_bd_addr(central, seen, bd_addr)?;
         }
         CentralEvent::DeviceDisconnected(bd_addr) => {
             println!("DeviceDisconnected: {:?}", bd_addr);
+            handle_bd_addr(central, seen, bd_addr)?;
         }
         CentralEvent::DeviceUpdated(bd_addr) => {
-            if !seen.contains(&bd_addr) {
-                let device = central
-                    .peripheral(bd_addr)
-                    .ok_or_else(|| anyhow!("missing peripheral {}", bd_addr))?;
-                let props = device.properties();
-
-                println!(
-                    "DeviceUpdated: {:?}, {:?}, {:?}",
-                    bd_addr,
-                    device.is_connected(),
-                    props,
-                );
-                seen.insert(bd_addr);
-
-                if props.local_name == Some("LYWSD03MMC".into()) {
-                    connect_sensor(&device)?;
-                    mijia::start_notify_sensor(&device)?;
-                }
-            }
+            handle_bd_addr(central, seen, bd_addr)?;
         }
         e => {
             println!("Other event {:?}", e);
+        }
+    }
+    Ok(())
+}
+
+fn handle_bd_addr(
+    central: &ConnectedAdapter,
+    seen: &mut HashSet<BDAddr>,
+    bd_addr: BDAddr,
+) -> anyhow::Result<()> {
+    if !seen.contains(&bd_addr) {
+        let device = central
+            .peripheral(bd_addr)
+            .ok_or_else(|| anyhow!("missing peripheral {}", bd_addr))?;
+        let props = device.properties();
+
+        println!(
+            "new bluetooth address: {:?}, {:?}, {:?}",
+            bd_addr,
+            device.is_connected(),
+            props,
+        );
+        seen.insert(bd_addr);
+
+        if props.local_name == Some("LYWSD03MMC".into()) {
+            connect_sensor(&device)?;
+            mijia::start_notify_sensor(&device)?;
         }
     }
     Ok(())
